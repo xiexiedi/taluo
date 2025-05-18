b03d6bd034e523609bd3326b567641465126c80c
xiexiedi <168257697+xiexiedi@users.noreply.github.com>
2025-05-18 11:49:25 +0800

    Updated LoadingSpinner.tsx

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Player } from '@lottiefiles/react-lottie-player';
+import Lottie from 'lottie-react';
 import tarotCardsAnimation from '/素材库/tarot-cards.json';
 
 export const LoadingSpinner: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-[200px]">
       <div className="w-32 h-32">
-        <Player
-          src={tarotCardsAnimation}
-          loop
-          autoplay
+        <Lottie
+          animationData={tarotCardsAnimation}
+          loop={true}
+          autoplay={true}
           style={{ width: '100%', height: '100%' }}
         />
       </div>
