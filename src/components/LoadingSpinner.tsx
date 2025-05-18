66885324cd3225378998aafe19a1186f916ce717
xiexiedi <168257697+xiexiedi@users.noreply.github.com>
2025-05-18 13:07:02 +0800

    Fix JSON parsing error

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Lottie from 'lottie-react';
-import tarotCardsAnimation from '/素材库/tarot-cards.json';
+import tarotCardsAnimation from '../assets/animations/tarot-cards.json';
 
 export const LoadingSpinner: React.FC = () => {
   return (
@@ -9,7 +9,7 @@ export const LoadingSpinner: React.FC = () => {
         <Lottie
           animationData={tarotCardsAnimation}
           loop={true}
-          style={{ filter: 'brightness(0) invert(1)' }} // Make the animation white
+          style={{ filter: 'brightness(0) invert(1)' }}
         />
       </div>
       <p className="text-white/80 mt-4 animate-pulse">加载中...</p>
